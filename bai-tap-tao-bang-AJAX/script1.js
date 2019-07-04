457ae033a1c69e2867e6d11b050cd30823a761c3
ngovansonWFT1 <ngoson2910@gmail.com>
2019-07-04 12:19:54 +0700

    sửa

--- bai-tap-tao-bang-AJAX/script1.js
+++ bai-tap-tao-bang-AJAX/script1.js
@@ -3,6 +3,7 @@ $.ajax({
     type: 'get',
     dataType: 'json',
 }).done(function (listSanPham) {
+    console.log(listSanPham)
     function formatNumber(num) {
         return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
     }
@@ -92,14 +93,14 @@ $.ajax({
 
         if ($($('th')[4]).attr('data-order') === 'desc') {
             $($('th')[4]).attr('data-order', 'asc');
-            sortAzZa($($('th')[4]).attr('data-column'), $($('th')[4]).attr('data-order'));
+            sortNum($($('th')[4]).attr('data-column'), $($('th')[4]).attr('data-order'));
             $($('th')[4]).children().removeClass('fa-sort').addClass('fa-sort-up');
             $($('th')[4]).children().removeClass('fa-sort-down');
 
 
         } else {
             $($('th')[4]).attr('data-order', 'desc');
-            sortAzZa($($('th')[4]).attr('data-column'), $($('th')[4]).attr('data-order'));
+            sortNum($($('th')[4]).attr('data-column'), $($('th')[4]).attr('data-order'));
             $($('th')[4]).children().removeClass('fa-sort-up').addClass('fa-sort-down');
 
         }
@@ -130,13 +131,18 @@ $.ajax({
         listSanPham.sort(function (a, b) {
             let x = a[column];
             let y = b[column];
-            if (typeof a[column] == 'string') x = a[column].toLowerCase();
-            if (typeof b[column] == 'string') y = b[column].toLowerCase();
+            if (typeof a[column] == 'string')  {x = a[column].toLowerCase()} ;
+            if (typeof b[column] == 'string')  {y = b[column].toLowerCase()};
             if (x > y) return 1;
             if (x < y) return -1;
             return 0;
 
         });
+        if (data == 'asc') return listSanPham;
+        else return listSanPham.reverse();
+    }
+    function sortNum(column, data) {
+        listSanPham.sort((a, b) => a[column]-b[column]);
         if (data == 'asc') return listSanPham;
         else return listSanPham.reverse();
     }
