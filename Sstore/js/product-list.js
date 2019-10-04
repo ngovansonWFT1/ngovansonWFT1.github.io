72d8ecbe1953409a87566a78f0d1084ce942c508
Ngoson <ngoson2910@gmail.com>
2019-10-04 20:18:58 +0700

    final

--- Sstore/js/product-list.js
+++ Sstore/js/product-list.js
@@ -11,8 +11,8 @@ $(function () {
     $('.list-icon').click(function () {
         $('.grid-product-list').addClass('hidden');
         $('.list-product-appearence').addClass('showed')
-        $(this).children().attr('src', './../images/product-list/list-active.svg');
-        $('.grid-icon img').attr('src', './../images/product-list/grid-none-active.svg')
+        $(this).children().attr('src', './images/product-list/list-active.svg');
+        $('.grid-icon img').attr('src', './images/product-list/grid-none-active.svg')
         $(this).addClass('add-background');
         $('.layout-and-sort .grid-icon').addClass('remove-background');
     })
@@ -20,8 +20,8 @@ $(function () {
     $('.grid-icon').click(function () {
         $('.grid-product-list').removeClass('hidden');
         $('.list-product-appearence').removeClass('showed');
-        $(this).children().attr('src', './../images/product-list/grid-active.svg');
-        $('.list-icon img').attr('src', './../images/product-list/list-disactive.svg')
+        $(this).children().attr('src', './images/product-list/grid-active.svg');
+        $('.list-icon img').attr('src', './images/product-list/list-disactive.svg')
         $(this).removeClass('remove-background');
         $('.layout-and-sort .list-icon').removeClass('add-background');
     })
